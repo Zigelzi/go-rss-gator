669f440ffe2279877fa87288313711cf298b492f
zigelzi <miika.a.savela@gmail.com>
2025-07-28 13:47:03 +0300

    Add handling for different timestamp formats

--- feed_printer.go
+++ feed_printer.go
@@ -16,7 +16,7 @@ func printFeedContent(rssFeed *rss.RSSFeed) {
 		fmt.Println(item.Title)
 		fmt.Println(item.Description)
 		fmt.Printf("Read more: %s\n", item.Link)
-		pubTimestamp, err := time.Parse(time.RFC1123, item.PublishDate)
+		pubTimestamp, err := parseTimestamp(item.PublishDate)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -26,4 +26,21 @@ func printFeedContent(rssFeed *rss.RSSFeed) {
 		fmt.Println(strings.Repeat("-", 30))
 		fmt.Println()
 	}
+}
+
+func parseTimestamp(timestampStr string) (time.Time, error) {
+	formats := []string{
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC3339,
+		time.RFC822,
+		time.RFC822Z,
+	}
+
+	for _, format := range formats {
+		if timestamp, err := time.Parse(format, timestampStr); err == nil {
+			return timestamp, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("unable to parse timestamp: %s", timestampStr)
 }
